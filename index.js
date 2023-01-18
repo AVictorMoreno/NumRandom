018ea2725edd02ebc9637fddcc9db6ab0c94551a
AVictorMoreno <a20vicmormor@inspedralbes.cat>
2023-01-18 14:58:19 +0500

    fet

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const port = process.env.PORT || 3000;
+const port = 7089;
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -10,9 +10,13 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
   socket.on('chat message', msg => {
     io.emit('chat message', msg);
+    });
+    console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
   });
 });
 
 http.listen(port, () => {
-  console.log(`Socket.IO server running at http://localhost:${port}/`);
+  console.log(`Socket.IO server running at ${port}`);
 });
